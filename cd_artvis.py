577734384c333ad4d82409d0abacf839db0aa046
sugita <sugita5019@gmail.com>
2020-02-25 23:23:22 +0800

    updated

--- cd_artvis.py
+++ cd_artvis.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-imax = 101
+imax = 1001
 dx = 10.0/(imax-1)
 u = np.ndarray((imax),dtype=np.float64)
 x = np.ndarray((imax),dtype=np.float64)
@@ -22,7 +22,8 @@ plt.show()
 un = u #initiate new value for u
 dt = np.float64(input("Enter dt, dx=%s\n  "%dx ))
 
-itermax = int( 2.0/dt ) 
+itermax = int( 1.0/dt ) 
+
 c = 1.0
 alpha = 0.5*c*dt/dx
 beta = ( 0.5*c*dt/dx )
@@ -35,26 +36,36 @@ for iter in range(itermax):
         denominator = u[i+1] - u[i]
         numerator = u[i]-u[i-1]
         ratio = numerator / (denominator + 0.001)
-        if denominator == 0.0:
-            mm = 0.0
-        if abs(ratio) > 0.0:
+        mm = 0.0
+        if abs(ratio) > 1.0:
             mm = 1.0
+        else:
+            mm = 0.0 #ratio
 
         un[i] = u[i] - alpha*( u[i+1] - u[i-1] ) + mm*beta*(u[i-1] - 2.0*u[i] + u[i+1] )
+        #un[i] = u[i] - 2.0*alpha*( u[i] - u[i-1] ) #1st upwind
 
     #update
     u = un
-    plt.axis([0.0, 11.0, -0.5, 1.5 ] )
+    current = iter*dt + dt
+    display = "t = %.4f"%(current)
+    plt.axis([0.0, 10.0, -0.5, 1.5 ] )
+    plt.title(display)
+    plt.ylabel("U")
+    plt.xlabel("x")
     plt.plot(x,u,'bo-')
-    plt.pause(0.1)
+    plt.pause(0.001)
     plt.clf() #clear drawing
     
 
-
-
-plt.axis([0.0, 11.0, -0.5, 1.5 ] )
+#filename= "t%.2f"%(current) + ".png"
+filename = "final.png"
+plt.axis([0.0, 10.0, -0.5, 1.5 ] )
 plt.plot(x,u, 'bo-')
-plt.title("After advection")
-plt.ylabel("u")
+plt.title(display)
+plt.ylabel("U")
+plt.xlabel("x")
+plt.savefig(filename)
 plt.show()
+
 
